e4285d6368f3a3c26a6bdcaa9bfedea9d0c7992e
CauaFelipe25 <caua.7843114@aluno.mg.gov.br>
2026-04-08 16:38:14 +0000

    auteração 1

---
+++ function boasVindas() {.js
@@ -0,0 +1,24 @@
+function boasVindas() {
+    alert("Bem-vindo ao site sobre Bangkok!");
+}
+
+function mostrarTexto() {
+    var texto = document.getElementById("extra");
+
+    if (texto.style.display === "none") {
+        texto.style.display = "block";
+    } else {
+        texto.style.display = "none";
+    }
+}
+
+function mudarCor() {
+    document.body.style.backgroundColor = "#dff9fb";
+}
+
+var numero = 0;
+
+function contador() {
+    numero++;
+    document.getElementById("numero").innerText = numero;
+}
